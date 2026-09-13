c7ed8f46790aee5763a1e5430c06a2c2a5e905fe
Tyler Seth Duggan <163541397+FireTitan29@users.noreply.github.com>
2026-09-13 13:30:29 +0200

    Pantry Controller - GetAll - DeleteItem
    Pantry Controller has gotten it's two functions I have planned for it.

--- app/src/main/java/com/example/smartpantrymanager/common/MeasurementConverter.java
+++ app/src/main/java/com/example/smartpantrymanager/common/MeasurementConverter.java
@@ -1,4 +1,67 @@
 package com.example.smartpantrymanager.common;
 
-public class MeasurementConverter {
+import com.example.smartpantrymanager.models.AppSettings;
+import com.example.smartpantrymanager.services.AppSettingsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class MeasurementConverter {
+
+    private MeasurementConverter() {
+
+    }
+
+    public static float convert(String measurement, AppSettingsService appSettingsService, float value) throws Exception {
+
+        if (measurement.equals("Units")) {
+            return value;
+        }
+
+        AppSettings appSettings = appSettingsService.getAppSettings();
+
+        List<String> weight = List.of("kg", "g", "mg");
+        List<String> volume = List.of("l", "ml");
+
+
+        int measurement_index;
+        int appsetting_index;
+
+        // Convert Weight
+        if (weight.contains(measurement)) {
+
+            measurement_index = weight.indexOf(measurement);
+            appsetting_index = weight.indexOf(appSettings.getWeightUnit());
+
+        } else {
+
+            measurement_index = volume.indexOf(measurement);
+            appsetting_index = volume.indexOf(appSettings.getVolumeUnit());
+        }
+
+        int modifyBy = 0;
+
+        // Multiply / divide by 1000 to get to the correct measurement value
+
+        if (measurement_index <= appsetting_index) {
+
+            modifyBy = appsetting_index - measurement_index;
+
+            for (int i = 0; i < modifyBy; i++) {
+
+                value *= 1000;
+            }
+
+        } else {
+
+            modifyBy = measurement_index - appsetting_index;
+
+            for (int i = 0; i < modifyBy; i++) {
+
+                value /= 1000;
+            }
+        }
+
+        return value;
+    }
 }
--- app/src/main/java/com/example/smartpantrymanager/controllers/PantryController.java
+++ app/src/main/java/com/example/smartpantrymanager/controllers/PantryController.java
@@ -1,6 +1,83 @@
 package com.example.smartpantrymanager.controllers;
 
+import android.util.Log;
+
+import com.example.smartpantrymanager.common.MeasurementConverter;
+import com.example.smartpantrymanager.database.DatabaseHelper;
+import com.example.smartpantrymanager.models.AppSettings;
+import com.example.smartpantrymanager.models.Measurement;
+import com.example.smartpantrymanager.models.PantryItem;
+import com.example.smartpantrymanager.services.AppSettingsService;
+import com.example.smartpantrymanager.services.MeasurementService;
+import com.example.smartpantrymanager.services.PantryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PantryController {
+
+    private PantryService pantryService;
+    private AppSettingsService appSettingsService;
+    private MeasurementService measurementService;
+
+    // Constructor
+    public PantryController(DatabaseHelper databaseHelper) {
+        this.pantryService = new PantryService(databaseHelper);
+        this.appSettingsService = new AppSettingsService(databaseHelper);
+        this.measurementService = new MeasurementService(databaseHelper);
+    }
+
+    // Functions
+
+    // Gets pantry items and converts them to the measurements in the db to the measurement
+    // preference of the user. Db measurements are stored in ml and g
+    public List<PantryItem> getPantryItems() {
+
+        List<PantryItem> items = new ArrayList<>();
+
+        List<Measurement> measurements = measurementService.getAllMeasurements();
+
+        try {
+
+            items = pantryService.getAllPantryItems();
+
+            for (int i = 0; i < items.size(); i ++) {
+
+                PantryItem item = items.get(i);
+
+                Measurement item_measurement = measurements.stream()
+                        .filter(m -> m.getPrimaryKey() == item.getMeasurementId())
+                        .findFirst()
+                        .orElseThrow();
+
+                float newValue = MeasurementConverter.convert(item_measurement.getAbbreviation(), appSettingsService, item.getQuantity());
+
+                item.setQuantity(newValue);
+
+            }
+
+            return items;
+
+        } catch (Exception ex) {
+
+            Log.d("GetPantryItems", ex.toString());
+            return items;
+        }
+    }
+
+    public boolean deletePantryItem(int primaryKey) {
+
+        try {
+
+            pantryService.deletePantryItem(primaryKey);
+            return true;
+
+        } catch (Exception ex) {
+
+            Log.d("GetPantryItems", ex.toString());
+            return false;
+        }
+    }
 
 
 
